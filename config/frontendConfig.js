26500c96ca70dab003d2db72535c4fd2b1e87c4b
fahrioghanial <fahrioghanial@gmail.com>
2022-03-26 22:17:35 +0700

    add login and signup feature

---
+++ config/frontendConfig.js
@@ -0,0 +1,17 @@
+import ThirdPartyEmailPasswordReact from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import SessionReact from "supertokens-auth-react/recipe/session";
+import { appInfo } from "./appInfo";
+
+export let frontendConfig = () => {
+  return {
+    appInfo,
+    recipeList: [
+      ThirdPartyEmailPasswordReact.init({
+        emailVerificationFeature: {
+          mode: "REQUIRED",
+        },
+      }),
+      SessionReact.init(),
+    ],
+  };
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { useEffect } from "react";
+import SuperTokensReact from "supertokens-auth-react";
+import * as SuperTokensConfig from "../config/frontendConfig";
+import Session from "supertokens-auth-react/recipe/session";
+import { redirectToAuth } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (typeof window !== "undefined") {
+  SuperTokensReact.init(SuperTokensConfig.frontendConfig());
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    async function doRefresh() {
+      if (pageProps.fromSupertokens === "needs-refresh") {
+        if (await Session.attemptRefreshingSession()) {
+          location.reload();
+        } else {
+          // user has been logged out
+          redirectToAuth();
+        }
+      }
+    }
+    doRefresh();
+  }, [pageProps.fromSupertokens]);
+  if (pageProps.fromSupertokens === "needs-refresh") {
+    return null;
+  }
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
