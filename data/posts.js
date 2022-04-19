2a5c6a7380bd984702ae48d4e39012d17efba971
fahrioghanial <fahrioghanial@gmail.com>
2022-04-19 13:44:39 +0700

    add create post page

---
+++ data/posts.js
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    id: 1,
+    title: "first title",
+    tag: "first tag",
+    content: "first content",
+  },
+  {
+    id: 2,
+    title: "second title",
+    tag: "second tag",
+    content: "second content",
+  },
+  {
+    id: 3,
+    title: "third title",
+    tag: "third tag",
+    content: "third content",
+  },
+];
